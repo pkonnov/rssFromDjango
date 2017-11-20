03a0f09d30f5321cbace7a8cb1354f8566358876
cretyne <4itonada@gmail.com>
2017-11-20 21:19:37 +0400

    make two model db

--- rss/apps/rss_news/admin.py
+++ rss/apps/rss_news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from rss.apps.rss_news.models import Post
 
 # Register your models here.
+
+admin.site.register(Post)
--- rss/apps/rss_news/models.py
+++ rss/apps/rss_news/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     id = models.BigIntegerField(primary_key=True)
-    title = models.TextField()
+    title = models.CharField(max_length=60)
     text = models.TextField()
 
     def __str__(self):
