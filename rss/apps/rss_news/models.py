d956793ac9f1fd8df087992ffc8ad53c2209af6b
cretyne <4itonada@gmail.com>
2017-11-21 21:06:48 +0400

    migration datetime.models

--- rss/apps/rss_news/models.py
+++ rss/apps/rss_news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Post(models.Model):
     id = models.BigIntegerField(primary_key=True)
     title = models.CharField(max_length=60)
     text = models.TextField()
+    date = models.DateField(default=str(datetime.datetime.now())[:10])
 
     def __str__(self):
         return str(self.id) + "_" + str(self.title)
